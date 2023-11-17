c021495e757838b1d63cf37a7a8f2dca08bc94ac
MaksGorobets <gorobets542@gmail.com>
2023-11-17 20:31:08 +0200

    Initial feature testing

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,16 +7,78 @@
 
 import SwiftUI
 
+struct User: Codable, Hashable {
+    var name: String
+    var secondName: String
+}
+
 struct ContentView: View {
+    
+    @State private var users = [User]()
+    @State private var showingSheet = false
+    
+    @State private var newName = ""
+    @State private var newSecondName = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(users, id: \.self) {
+                    Text("\($0.name) \($0.secondName)")
+                }
+                .onDelete(perform: deleteItem)
+            }
+                Button("Add a person") {
+                    showingSheet.toggle()
+                }
+            .sheet(isPresented: $showingSheet) {
+                Spacer()
+                VStack {
+                    TextField("Enter a name", text: $newName)
+                    TextField("Enter a second name", text: $newSecondName)
+                }
+                .padding()
+                Spacer()
+                Button("Done") {
+                    showingSheet = false
+                    users.append(User(name: newName, secondName: newSecondName))
+                }
+            }
+            .navigationTitle("iExpense Testing")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                EditButton()
+                Button("Save all") {
+                    saveData()
+                }
+            }
         }
-        .padding()
+        .onAppear {
+            loadData()
+        }
     }
+    
+    func deleteItem(on offsets: IndexSet) {
+        users.remove(atOffsets: offsets)
+    }
+    
+    func saveData() {
+        let encoder = JSONEncoder()
+        
+        if let data = try? encoder.encode(users) {
+            UserDefaults.standard.set(data, forKey: "data")
+        }
+    }
+    
+    func loadData() {
+        let decoder = JSONDecoder()
+        
+        if let data = UserDefaults.standard.data(forKey: "data"),
+           let decoded = try? decoder.decode([User].self, from: data) {
+            users = decoded
+        }
+    }
+    
 }
 
 #Preview {
